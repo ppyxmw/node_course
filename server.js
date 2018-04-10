ee00ffc1bc447c3a27d442b31afd8738d1926ce8
EC2 Default User <ec2-user@ip-172-31-34-74.eu-west-1.compute.internal>
2018-04-10 17:13:57 +0000

    adds projects view

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'All Projects'
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
